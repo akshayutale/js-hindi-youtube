295287a8fd315b16e58ec66221d806bd30be8c69
Akshay Anil Utale <35684805+akshayutale@users.noreply.github.com>
2022-12-25 16:05:26 +0000

    JS Variables

---
+++ 01Basics/01_Variables.js
@@ -0,0 +1,18 @@
+const accountId = 31281
+let emailId = "akshayutale7@gmail.com"
+var password = "Akshay@@!@#$"
+accountCity = "Mumbai"
+
+// accountId = 2 // This is not allowed
+
+emailId = "akshayraoutale@gmail.com"
+password = "Akshayutale@!@#$%"
+accountCity = "Pune"
+let accountState;
+
+console.log(accountId); 
+console.table([accountId, emailId, password, accountCity, accountState]) 
+
+/*
+Note: Prefer not to use var beacus of block socpe and functional scope
+*/
