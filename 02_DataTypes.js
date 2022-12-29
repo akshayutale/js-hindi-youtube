12d78f2d29d0fde022e9ad02569a3d659fb65667
Akshay Anil Utale <35684805+akshayutale@users.noreply.github.com>
2022-12-29 18:19:16 +0000

    Data Types

---
+++ 02_DataTypes.js
@@ -0,0 +1,29 @@
+"use strict";  // This mean treat all JS code as a newer version
+
+// alert(3 + 3) //  We are using Node JS and not browser so this is not allowed in node js
+
+console.log(3 
+    + 3)  // Code Readability should be high  --> This is not acceptable
+
+    console.log("Akshay")
+
+    // https://tc39.es/ecma262/  --> This web site to be followed
+
+    let Name = "Akshay"
+    let age = 18
+    // isLoggedIn = false
+
+   // number => 2 to power 53
+   // bigint 
+   // string => ""
+   // boolean => true / false
+   // null   --> This is stand alone value and in JS it is data type
+   // undefined => 
+   // symbol
+
+   // objects
+
+   console.log(typeof 3);
+   console.log(typeof null);
+   console.log(typeof undefined);
+   
